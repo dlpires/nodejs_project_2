93ace9066185cade320c14d0d2e955391e7bfd4e
Diego Luis Pires <diegoluispires@gmail.com>
2023-04-19 00:14:39 -0300

    Class 1 - Prepair and test node env

---
+++ database/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+//ACESSO DB
+const DATABASE = "nodeprojeto2";
+const DB_USER = "nodeproject1";
+const DB_PASSWORD = "N0d3J52320@";
+const DB_HOST = "localhost"
+
+const connection = new Sequelize(DATABASE,DB_USER,DB_PASSWORD,{
+    host: DB_HOST,
+    dialect: 'mysql',
+    /*dialectOptions: {
+        socketPath: '/var/run/mysqld/mysqld.sock'
+    }*/ // NECESSÁRIO PARA DEPLOY EM LINUX (DIGITAL OCEAN)
+});
+
+module.exports = connection;
