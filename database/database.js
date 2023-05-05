4ca9fafe0a20d6fbb18f80d0ec8d24213e0c59d1
Diego Luis Pires <diegoluispires@gmail.com>
2023-05-04 23:45:54 -0300

    Class 3 - Create CRUD Categories and Articles - Started (use TinyMCE)

--- database/database.js
+++ database/database.js
@@ -9,6 +9,7 @@ const DB_HOST = "localhost"
 const connection = new Sequelize(DATABASE,DB_USER,DB_PASSWORD,{
     host: DB_HOST,
     dialect: 'mysql',
+    timezone: '-03:00'
     /*dialectOptions: {
         socketPath: '/var/run/mysqld/mysqld.sock'
     }*/ // NECESSÁRIO PARA DEPLOY EM LINUX (DIGITAL OCEAN)
