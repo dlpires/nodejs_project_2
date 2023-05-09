87c2c08123c67d1a6f465a7f8ccc15aafb2ba472
Diego Luis Pires <diegoluispires@gmail.com>
2023-05-08 23:19:38 -0300

    Class 4 - Create homepage, complete article CRUD (update)

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -58,4 +58,44 @@ router.post("/articles/delete", (req,res) => {
     }
 });
 
+//ROUTE TO EDIT A ARTICLE
+router.get("/admin/articles/edit/:id", (req,res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {article:article,categories:categories});
+            });
+        }
+        else {
+            res.redirect("/admin/articles");
+        }
+
+    }).catch(e => {
+        res.redirect("/admin/articles");
+    });
+});
+
+//ROUTE TO SAVE ARTICLE EDITION
+router.post("/articles/update", (req,res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title,body: body,slug: slugify(title),categoryId: category},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
+
+
 module.exports = router;
